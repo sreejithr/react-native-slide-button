a0599d17dad127ce17109df025662d1b48d8b116
Sreejith R <sreejith.r44@gmail.com>
2016-09-29 17:31:39 +0530

    Moved example logic to a separate file. Added button supporting sliding in both directions

--- Examples/SlideButtonExample/index.ios.js
+++ Examples/SlideButtonExample/index.ios.js
@@ -1,87 +1,10 @@
 /**
  * Slide Button Example App
- */
+ **/
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-import { SlideButton } from 'react-native-slide-button';
-
-var Dimensions = require('Dimensions');
-var SCREEN_WIDTH = Dimensions.get('window').width;
-
-class SlideButtonExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      swiped: false
-    };
-  }
-
-  onSlide() {
-    this.setState({swiped: !this.state.swiped});
-  }
-
-  render() {
-    var message = (
-        <Text style={{paddingBottom: 20, color: '#0FACF3'}}>
-          Please Slide Button
-        </Text>
-    );
-
-    if (this.state.swiped) {
-      message = (
-          <Text style={{paddingBottom: 20, color: 'green'}}>
-            Slide Success!
-          </Text>
-      );
-    }
-
-    return (
-      <View style={styles.container}>
-        <View>
-          {message}
-        </View>
-
-        <View style={styles.buttonOuter}>
-          <SlideButton onSlideSuccess={this.onSlide.bind(this)}>
-            <View style={styles.buttonInner}>
-              <Text style={styles.button}>Slide Button</Text>
-            </View>
-          </SlideButton>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  buttonOuter: {
-    backgroundColor: '#0FACF3',
-    height: 50,
-    width: SCREEN_WIDTH-40
-  },
-  buttonInner: {
-    width: SCREEN_WIDTH-40,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  button: {
-    color: 'white',
-    fontWeight: 'bold'
-  }
-});
+import { AppRegistry } from 'react-native';
+import SlideButtonExample from './SlideButtonExample';
 
 AppRegistry.registerComponent('SlideButtonExample', () => SlideButtonExample);
